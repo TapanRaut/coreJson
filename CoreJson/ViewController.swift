45cb7818d9ec8436e3046773e7437b28febc7fb1
Tapan Raut <rinku.tapan.raut@gmail.com>
2023-09-23 19:40:35 +0530

    Initial Commit

---
+++ CoreJson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreJson
+//
+//  Created by Tapan Raut on 23/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
